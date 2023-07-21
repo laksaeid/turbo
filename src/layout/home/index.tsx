b0e2b1f32f8041a81ffd3378b2cdef5ba297d106
laksaeid <laksaeidd2@gmail.com>
2023-07-21 21:03:26 +0330

    add home layout component

---
+++ src/layout/home/index.tsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+const HomeLayout = () => {
+    return ( 
+        <>
+        <div>asd</div>
+        <Outlet/>
+        </>
+     );
+}
+ 
+export default HomeLayout;
