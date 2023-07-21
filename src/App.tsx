ef290301a6f46a160ea144d2104bef65d8397e01
laksaeid <laksaeidd2@gmail.com>
2023-07-21 21:52:33 +0330

    add queryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { Container } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  const queryClient = new QueryClient()
   return (
     <>
+    <QueryClientProvider client={queryClient} >
       <Container maxWidth="xl">
         <RouterProvider router={router} />
       </Container>
+    </QueryClientProvider>
     </>
   );
 }
