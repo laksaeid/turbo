c1f6036b0d2f3c893ee23844e856dae97ab43d16
laksaeid <laksaeidd2@gmail.com>
2023-07-21 22:07:19 +0330

    edit useAddTodo hook

--- src/hooks/useAddTodo/index.ts
+++ src/hooks/useAddTodo/index.ts
@@ -1,11 +1,15 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import { instance } from "../../constants";
 
-const useAddTodo = () => {
-return useMutation({
-    mutationFn:(data:{todo:string})=>instance.post('',data),
-})
+const useAddTodo = (reset:()=>void) => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: async (data: { todo: string }) => await instance.post("", data),
+    onSuccess: () => {
+      queryClient.invalidateQueries(["getTodos"]);
+      reset();
+    },
+  });
+};
 
-}
- 
 export default useAddTodo;
