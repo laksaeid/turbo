4ec20982a665e689489fe099e85a83b1872fe3e7
laksaeid <laksaeidd2@gmail.com>
2023-07-21 21:27:49 +0330

    fix layout styles

--- src/layout/home/index.tsx
+++ src/layout/home/index.tsx
@@ -1,12 +1,22 @@
+import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
-    return ( 
-        <>
-        <div>asd</div>
-        <Outlet/>
-        </>
-     );
-}
- 
+  return (
+    <Box sx={{ height: "100vh", display: "flex", flexDirection: "column" }}>
+      <div>asd</div>
+      <Box
+        sx={{
+          flex: "1 1 0%",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <Outlet />
+      </Box>
+    </Box>
+  );
+};
+
 export default HomeLayout;
