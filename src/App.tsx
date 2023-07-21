c822a5b350e632f89fe56abd432f3c8d400726f5
laksaeid <laksaeidd2@gmail.com>
2023-07-21 21:03:45 +0330

    add container

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import { Container } from "@mui/material";
 
 function App() {
-
-
   return (
     <>
-    <div>
-      asd
-    </div>
+      <Container maxWidth="xl">
+        <RouterProvider router={router} />
+      </Container>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
